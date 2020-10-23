eee70ef07c987b00801b7252eaa883a2d52fc565
tuan.domanh <tuan.domanh>
2020-10-23 14:54:45 +0700

    update code

---
+++ test/src/test2/DongHo.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test2;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author tuand
+ */
+public class DongHo implements  Serializable{
+    String ten;
+    double gia;
+    String hang;
+
+    public DongHo(String ten, double gia, String hang) {
+        this.ten = ten;
+        this.gia = gia;
+        this.hang = hang;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public double getGia() {
+        return gia;
+    }
+
+    public void setGia(double gia) {
+        this.gia = gia;
+    }
+
+    public String getHang() {
+        return hang;
+    }
+
+    public void setHang(String hang) {
+        this.hang = hang;
+    }
+    
+}
